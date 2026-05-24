6e80e3a4a99a79d6e7c17e438c7fdf9b794f89a8
AhmedBenAbdallahDev <ahmeddev@email.com>
2026-05-24 22:14:52 +0100

    Remove Liquid Glass component and related assets, update site manifest name

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LiquidGlass } from "@/components/ui/liquid-glass";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen w-full overflow-hidden">
-      <LiquidGlass />
-    </main>
-  );
-}
